98fe2d421ae9b4e45750227f90cecc2b24d50d3a
notvillager <crzydev1245@gmail.com>
2021-07-28 09:12:09 +0700

    Add files via upload

---
+++ actors/thwomp/geo.inc.c
@@ -0,0 +1,15 @@
+// 0x0C000248
+/********************************************************************************
+		RCP_HmsDosun
+														[ Oct 23, 1995 ]
+*********************************************************************************/
+
+extern Gfx gfx_dosun[];
+
+const GeoLayout thwomp_geo[] = {
+   GEO_SHADOW(SHADOW_SQUARE_SCALABLE, 0xB4, 300),
+   GEO_OPEN_NODE(),
+      GEO_DISPLAY_LIST(LAYER_OPAQUE, gfx_dosun),
+   GEO_CLOSE_NODE(),
+   GEO_END(),
+};
