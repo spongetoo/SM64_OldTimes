496397233afe6816a0ddba36a30b4bdc9f59ea3f
what <70660993+noname64212@users.noreply.github.com>
2021-08-26 14:59:15 -0700

    Update bowser_key.inc.c

--- src/game/behaviors/bowser_key.inc.c
+++ src/game/behaviors/bowser_key.inc.c
@@ -1,4 +1,5 @@
 // bowser_key.c.inc
+//! Need to fix collision. the collision fucks up for some reason.
 
 struct ObjectHitbox sBowserKeyHitbox = {
     /* interactType: */ INTERACT_STAR_OR_KEY,
@@ -14,18 +15,12 @@ struct ObjectHitbox sBowserKeyHitbox = {
 
 void bhv_bowser_key_loop(void) {
     cur_obj_scale(0.5f);
-    if (o->oAngleVelYaw > 0x400)
-        o->oAngleVelYaw -= 0x100;
-    o->oFaceAngleYaw += o->oAngleVelYaw;
     o->oFaceAngleRoll = -0x4000;
     o->oGraphYOffset = 165.0f;
     if (o->oAction == 0) {
         if (o->oTimer == 0)
-            o->oVelY = 70.0f;
-        spawn_sparkle_particles(3, 200, 80, -60);
-        spawn_object(o, MODEL_NONE, bhvSparkleSpawn);
-        cur_obj_update_floor_and_walls();
-        cur_obj_move_standard(78);
+       // spawn_sparkle_particles(3, 200, 80, -60);
+       // spawn_object(o, MODEL_NONE, bhvSparkleSpawn);
         if (o->oMoveFlags & OBJ_MOVE_ON_GROUND)
             o->oAction++;
         else if (o->oMoveFlags & OBJ_MOVE_LANDED)
